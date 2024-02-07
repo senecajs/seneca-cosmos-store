a00ba7da1c33cb732a71f8534be16f77a0c93903
Alex <jk1143298@gmail.com>
2024-02-07 16:23:33 +0100

    refactor tests: use let, const

--- test/cosmos-store.test.js
+++ test/cosmos-store.test.js
@@ -78,7 +78,7 @@ let containerConfig = {
 lab.test('validate', PluginValidator(Plugin, module))
 
 lab.test('happy', async () => {
-  var si = make_seneca()
+  const si = make_seneca()
   await si.ready()
   expect(si.find_plugin('cosmos-store$1')).exists()
 
@@ -89,7 +89,7 @@ lab.test('happy', async () => {
 })
 
 lab.test('no-dups', async () => {
-  var si = make_seneca()
+  const si = make_seneca()
   await si.ready()
   si.quiet()
 
@@ -526,10 +526,10 @@ lab.test('injection-fails', async () => {
 })
 
 lab.test('export', async () => {
-  var si = make_seneca()
+  const si = make_seneca()
   await si.ready()
 
-  var get_client = si.export('cosmos-store$1/get_client')
+  const get_client = si.export('cosmos-store$1/get_client')
   expect(get_client()).exists()
 })
 
@@ -568,18 +568,18 @@ lab.describe('legacy-store-test', () => {
 
 lab.test('store-core', async () => {
   const plugin = {}
-  var si = make_seneca({ plugin })
+  const si = make_seneca({ plugin })
   await testrun.store_core({ seneca: si, expect, xlog: console.log })
 })
 
 lab.test('store-load', async () => {
   const plugin = {}
-  var si = make_seneca({ plugin })
+  const si = make_seneca({ plugin })
   await testrun.store_load({ seneca: si, expect, xlog: console.log })
 })
 
 lab.test('store-save', async () => {
-  var plugin = {
+  const plugin = {
     entity: {
       'test/foo': {
         // for special handling
@@ -592,13 +592,13 @@ lab.test('store-save', async () => {
     },
   }
 
-  var si = make_seneca({ plugin })
+  const si = make_seneca({ plugin })
   await testrun.store_save({ seneca: si, expect, xlog: console.log })
 })
 
 lab.test('custom-table', async () => {
   const plugin = {}
-  var si = make_seneca({ plugin })
+  const si = make_seneca({ plugin })
   let c0 = await si
     .entity('test/custom')
     .data$({ w: Date.now(), x: 1, y: 'a' })
@@ -632,26 +632,26 @@ lab.test('custom-table', async () => {
 
 const testrun = {
   store_core: async function (opts) {
-    var seneca = opts.seneca
-    var expect = opts.expect
-    var log = opts.log
+    const seneca = opts.seneca
+    const expect = opts.expect
+    const log = opts.log
 
     // S00010: Clear test/foo
     await seneca.entity('test/foo').remove$({ all$: true })
-    var foolist = await seneca.entity('test/foo').list$()
+    let foolist = await seneca.entity('test/foo').list$()
 
     log && log('S00010', foolist)
     expect(foolist.length).equal(0)
 
     // S00100: Load non-existent returns null.
-    var foo0n = await seneca.entity('test/foo').load$('not-an-id')
+    let foo0n = await seneca.entity('test/foo').load$('not-an-id')
 
     log && log('S00100', foo0n)
     expect(foo0n).equal(null)
 
     // S00200: Create unsaved entity
-    var m0 = (Math.random() + '').substring(2)
-    var foo0p = seneca.entity('test/foo').make$({
+    let m0 = (Math.random() + '').substring(2)
+    let foo0p = seneca.entity('test/foo').make$({
       m: m0,
       s: 's0',
       i: 0,
@@ -677,7 +677,7 @@ const testrun = {
     })
 
     // S00300: Save entity; generates id
-    var foo0 = await foo0p.save$()
+    let foo0 = await foo0p.save$()
 
     log && log('S00300', foo0)
     expect(foo0).exists()
@@ -712,7 +712,7 @@ const testrun = {
     ])
 
     // S00400: Load existing by id returns entity.
-    var foo0o = await seneca.entity('test/foo').load$(foo0.id)
+    let foo0o = await seneca.entity('test/foo').load$(foo0.id)
 
     log && log('S00400', foo0.id, foo0o)
     expect(foo0o).exists()
@@ -720,43 +720,43 @@ const testrun = {
     expect(foo0o.data$()).equals(foo0.data$())
 
     // S00500: List by query
-    var foolist0 = await seneca.entity('test/foo').list$({ m: m0 })
+    let foolist0 = await seneca.entity('test/foo').list$({ m: m0 })
 
     log && log('S00500', m0, foolist0)
     expect(foolist0.length).equals(1)
     expect(foolist0[0].data$()).equals(foo0o.data$())
 
     // S00600: Remove by id
-    var foo0ro = await seneca.entity('test/foo').remove$(foo0.id)
+    let foo0ro = await seneca.entity('test/foo').remove$(foo0.id)
 
     log && log('S00600', foo0.id, foo0ro)
     expect(foo0ro).equal(null)
 
     // S00700: Load by removed id
-    var foo0r = await seneca.entity('test/foo').load$(foo0.id)
+    let foo0r = await seneca.entity('test/foo').load$(foo0.id)
 
     log && log('S00700', foo0.id, foo0r)
     expect(foo0r).equal(null)
 
     // S00800: List removed by query returns []
-    var foolist0r = await seneca.entity('test/foo').list$({ m: m0 })
+    let foolist0r = await seneca.entity('test/foo').list$({ m: m0 })
 
     log && log('S00800', m0, foolist0r)
     expect(foolist0r.length).equals(0)
   },
 
   store_load: async function (opts) {
-    var seneca = opts.seneca
-    var expect = opts.expect
-    var log = opts.log
+    const seneca = opts.seneca
+    const expect = opts.expect
+    const log = opts.log
 
     // S01000: Load by field
-    var m1 = (Math.random() + '').substring(2)
-    var foo1 = await seneca
+    let m1 = (Math.random() + '').substring(2)
+    let foo1 = await seneca
       .entity('test/foo')
       .make$({ m: m1, s: 's1', i: 1, b: true })
       .save$()
-    var foo1o = await seneca.entity('test/foo').load$({ m: m1 })
+    let foo1o = await seneca.entity('test/foo').load$({ m: m1 })
 
     log && log('S01000', foo1, foo1o)
     expect(foo1).exists()
@@ -764,27 +764,27 @@ const testrun = {
     expect(foo1.data$()).equal(foo1o.data$())
 
     // S01100: Load by two fields
-    var foo1om = await seneca.entity('test/foo').load$({ m: m1, s: 's1' })
+    let foo1om = await seneca.entity('test/foo').load$({ m: m1, s: 's1' })
 
     log && log('S01100', foo1om)
     expect(foo1om).exists()
     expect(foo1.data$()).equal(foo1om.data$())
 
     // S01200: Load with no fields finds nothing
-    var foo1n = await seneca.entity('test/foo').load$({})
+    let foo1n = await seneca.entity('test/foo').load$({})
 
     log && log('S01200', foo1n)
     expect(foo1n).equal(null)
   },
 
   store_save: async function (opts) {
-    var seneca = opts.seneca
-    var expect = opts.expect
-    var log = opts.log
+    const seneca = opts.seneca
+    const expect = opts.expect
+    const log = opts.log
 
     // S10000: new entity: null saved, undefined ignored
-    var m0 = (Math.random() + '').substring(2)
-    var foo0 = await seneca
+    let m0 = (Math.random() + '').substring(2)
+    let foo0 = await seneca
       .entity('test/foo')
       .make$({
         m: m0,
@@ -794,7 +794,7 @@ const testrun = {
       })
       .save$()
 
-    var foo0o = await seneca.entity('test/foo').load$(foo0.id)
+    let foo0o = await seneca.entity('test/foo').load$(foo0.id)
 
     log && log('S10000', foo0, foo0o)
     expect(foo0o.data$(false)).equal({
@@ -805,8 +805,8 @@ const testrun = {
     })
 
     // S10100: existing entity: null saved, undefined ignored
-    var m1 = (Math.random() + '').substring(2)
-    var foo1 = await seneca
+    let m1 = (Math.random() + '').substring(2)
+    let foo1 = await seneca
       .entity('test/foo')
       .make$({
         m: m1,
@@ -814,10 +814,10 @@ const testrun = {
         s2: 's2~' + m1,
       })
       .save$()
-    var foo1o = await seneca.entity('test/foo').load$(foo1.id)
+    let foo1o = await seneca.entity('test/foo').load$(foo1.id)
     foo1o.s1 = null
     foo1o.s2 = undefined
-    var foo1o2 = await foo1o.save$()
+    let foo1o2 = await foo1o.save$()
 
     log && log('S10100', foo1, foo1o, foo1o2)
     expect(foo1o2.data$(false)).equal({
@@ -828,9 +828,9 @@ const testrun = {
     })
 
     // S10200: new item, edge cases: empty string, Date
-    var m2 = (Math.random() + '').substring(2)
-    var d1 = new Date()
-    var foo2 = await seneca
+    let m2 = (Math.random() + '').substring(2)
+    let d1 = new Date()
+    let foo2 = await seneca
       .entity('test/foo')
       .make$({
         m: m2,
@@ -850,9 +850,9 @@ const testrun = {
     expect(foo2.fields$()).equals(['m', 's1', 'd1', 'id'])
 
     // S10205: existing item, edge cases: empty string, Date
-    var d1a = new Date()
+    let d1a = new Date()
     foo2.d1 = d1a
-    var foo2o = await foo2.save$()
+    let foo2o = await foo2.save$()
 
     log && log('S10205', foo2)
     expect(foo2o.data$(false)).includes({
